b9c326c0d71b55db1968588d0faf9856919622c6
soredake <5204968+soredake@users.noreply.github.com>
2022-12-22 17:36:44 +0200

    улучшения

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+console.log("Часть 1:");
+
 const numbers = [3, 5, 11, 22, 55];
 
 for (let n in numbers) {
   console.log(`${numbers[n]} в квадрате это ${numbers[n] ** 2}`);
 }
 
+console.log("Часть 2:");
+
 const cities = {
-  киев: "Украина",
-  лондон: "Великобритания",
-  таллин: "Эстония",
+  Киев: "Украина",
+  Лондон: "Великобритания",
+  Таллин: "Эстония",
 };
 
 for (let c in cities) {
   console.log(`${c} это ${cities[c]}`);
 }
+
+console.log("Часть 3:");
 
 const numbersAgain = [2, 6, 5, 9, 15, 0, 10, 4];
 
