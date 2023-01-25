0f9046db8906beef669890bd0f3e5c24f390e741
soredake <5204968+soredake@users.noreply.github.com>
2023-01-25 17:13:53 +0200

    init

--- index.js
+++ index.js
@@ -1,30 +1,99 @@
-function sum(base = 0) {
-  return function (num2) {
-    base += num2;
-    console.log(base);
-  };
-}
+/** Get all tabs **/
+const $tabs = Array.from(document.querySelectorAll(".js-tab-item"));
+const activeClass = "active";
 
-let add = sum();
+$tabs.forEach(($tab) => {
+  $tab.addEventListener("click", function (e) {
+    $tabs.forEach(($tab) => {
+      const $link = $tab.querySelector("a");
+      $link.classList.remove(activeClass);
+    });
 
-add(3);
-add(5);
-add(20);
+    const $currentLink = e.target;
+    $currentLink.classList.add(activeClass);
+  });
+});
 
-const array = [1, 2, 3, 4, 5, 6, 7];
+/** ----------------------------------- **/
 
-// вариант подлиннее
-// function removeElement(array, element) {
-//   for (let i in array) {
-//     if (array[i] == element) {
-//       array.splice(i, 1);
-//     }
-//   }
-// }
+const $paginationItems = Array.from(
+  document.getElementsByClassName("js-pagination-item")
+);
+const $paginationButtonPrev = document.querySelector(".js-pagination-prev");
+const $paginationButtonNext = document.querySelector(".js-pagination-next");
+const disabledClass = "disabled";
 
-function removeElement(array, element) {
-  array.splice(array.indexOf(element), 1);
-}
+/** Remove all active classes from the pagination items**/
+const removeActiveClass = () => {
+  $paginationItems.forEach(($item) => $item.classList.remove(activeClass));
+};
 
-removeElement(array, 5);
-console.log(array);
+/** Toggle disabled class for the nav buttons **/
+const toggleDisabledButtons = () => {
+  const $activeItem = document.querySelector(".js-pagination-item.active");
+  const currentIndex = $paginationItems.indexOf($activeItem);
+  if (currentIndex + 1 === $paginationItems.length) {
+    $paginationButtonNext.classList.add(disabledClass);
+  } else {
+    $paginationButtonNext.classList.remove(disabledClass);
+  }
+  if (currentIndex === 0) {
+    $paginationButtonPrev.classList.add(disabledClass);
+  } else {
+    $paginationButtonPrev.classList.remove(disabledClass);
+  }
+};
+
+$paginationItems.forEach(($item) => {
+  $item.addEventListener("click", (e) => {
+    removeActiveClass();
+    const $target = e.target,
+      $parent = $target.closest(".js-pagination-item");
+    $parent.classList.add(activeClass);
+    toggleDisabledButtons();
+  });
+});
+
+$paginationButtonNext.addEventListener("click", () => {
+  if ($paginationButtonNext.classList.contains(disabledClass)) {
+    return;
+  }
+  const $activeItem = document.querySelector(".js-pagination-item.active");
+  const currentIndex = $paginationItems.indexOf($activeItem);
+  removeActiveClass();
+  $paginationButtonNext.classList.remove(disabledClass);
+  $paginationItems[currentIndex + 1].classList.add(activeClass);
+  toggleDisabledButtons();
+});
+
+$paginationButtonPrev.addEventListener("click", () => {
+  if ($paginationButtonPrev.classList.contains(disabledClass)) {
+    return;
+  }
+  const $activeItem = document.querySelector(".js-pagination-item.active");
+  const currentIndex = $paginationItems.indexOf($activeItem);
+  removeActiveClass();
+  $paginationButtonNext.classList.remove(disabledClass);
+  $paginationItems[currentIndex - 1].classList.add(activeClass);
+  toggleDisabledButtons();
+});
+
+/** ----------------------------------- **/
+
+const $form = document.querySelector(".js-form");
+const $formDataItems = Array.from(
+  document.getElementsByClassName("js-form-data")
+);
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const $inputs = Array.from($form.getElementsByTagName("input"));
+
+  for (let $item of $formDataItems) {
+    const dataAttr = $item.getAttribute("data-name");
+    const $currentInput = $inputs.find(
+      ($input) => $input.getAttribute("name") === dataAttr
+    );
+    $item.innerHTML = $currentInput.value;
+  }
+});
