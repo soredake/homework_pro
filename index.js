18f1a05b613f58bc5fd5165c571bf06c1b822dde
soredake <5204968+soredake@users.noreply.github.com>
2023-02-10 15:23:52 +0200

    Корректировки

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 "use strict";
+function getStringFromUser(message) {
+  let value;
+  do {
+    value = prompt(message);
+  } while (!value);
+  return value;
+}
+
 function removeSymbols(text, symbols) {
-  let editedText = text;
+  let editedText;
+  editedText = text;
   symbols.forEach((symbol) => {
     editedText = editedText.replaceAll(symbol, "");
   });
@@ -10,32 +19,35 @@ function removeSymbols(text, symbols) {
 function askForTextAndSymbols() {
   let text;
   let symbolsToDelete;
-  do {
-    text = prompt("Введите текст из которого мы будем удалять символы");
-  } while (!text);
-  do {
-    symbolsToDelete = prompt("Введите символы которые мы будем удалять").split(
-      ""
-    );
-  } while (symbolsToDelete.length <= 0);
+  let symbolsToDeleteArray;
+  text = getStringFromUser(
+    "Введите текст из которого мы будем удалять символы"
+  );
+  symbolsToDelete = getStringFromUser(
+    "Введите символы которые мы будем удалять"
+  );
+  symbolsToDeleteArray = symbolsToDelete.split("");
+  alert(removeSymbols(text, symbolsToDeleteArray));
+}
 
-  alert(removeSymbols(text, symbolsToDelete));
+function getNumberFromUser(message) {
+  let number;
+  do {
+    number = parseInt(prompt(message));
+  } while (!number || number < 0);
+  return number;
 }
 
 function twoDimensionalArray() {
   let mainArrayLength;
-  do {
-    mainArrayLength = parseInt(prompt("Введите длину основного массива"));
-  } while (!mainArrayLength || mainArrayLength < 0);
   let array = [];
+  mainArrayLength = getNumberFromUser("Введите длину основного массива");
   for (let arrayIndex = 0; arrayIndex < mainArrayLength; arrayIndex++) {
     let thisArrayLength;
-    do {
-      thisArrayLength = parseInt(
-        prompt(`Введите количество элементов в массиве ${arrayIndex + 1}`)
-      );
-    } while (!thisArrayLength || thisArrayLength < 0);
     let newArray = [];
+    thisArrayLength = getNumberFromUser(
+      `Введите количество элементов в массиве ${arrayIndex + 1}`
+    );
     for (let elementIndex = 0; elementIndex < thisArrayLength; elementIndex++) {
       let element = prompt(
         `Введите значение элемента ${elementIndex + 1} в массиве ${
