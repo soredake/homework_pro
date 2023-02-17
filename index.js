b92709742c0bc1f8a13d532dd5f15d51d7a7490f
soredake <5204968+soredake@users.noreply.github.com>
2023-02-17 20:55:11 +0200

    правки

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ document
   .querySelector('input[type="button"]')
   .addEventListener("click", function () {
     const results = document.getElementsByClassName("results")[0];
-    let registration_results = document.getElementsByClassName(
+    const registration_results = document.getElementsByClassName(
       "registration_results"
     )[0];
     const form = document.forms.registration;
@@ -33,14 +33,6 @@ document
         checkedLanguages.push(languagesObj[languagesElems[index].value]);
       }
     }
-
-    // console.log(`Имя: ${form.elements.name.value}`);
-    // console.log(`Фамилия: ${form.elements.surname.value}`);
-    // console.log(`Дата рождения: ${form.elements.birthday.value}`);
-    // console.log(`Пол: ${genderObj[form.elements.gender.value]}`);
-    // console.log(`Языки: ${checkedLanguages.join(", ")}`);
-    // console.log(`Адрес: ${form.elements.address.value}`);
-    // console.log(`Город: ${city.options[city.selectedIndex].textContent}`);
 
     if (registration_results.innerHTML) {
       registration_results.innerHTML = "";
