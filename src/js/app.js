985b723e3841d90433681e448c400334377e267c
soredake <5204968+soredake@users.noreply.github.com>
2023-02-21 17:08:38 +0200

    хмммм

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,46 @@
 "use strict";
+
+const images = ["image1", "image2", "image3", "image4", "image5", "image6"];
+let currentIndex = 1;
+
+document.querySelector("main").addEventListener(
+  "click",
+  function (event) {
+    const action = event.target.getAttribute("data-action");
+    const numberOfImages = images.length;
+    const imageItem = document.querySelector("img");
+    const prevButton = document.querySelector("[data-action=prev]");
+    const nextButton = document.querySelector("[data-action=next]");
+
+    if (!action) {
+      return;
+    }
+
+    if (action === "prev") {
+      if (currentIndex === 1) {
+        return;
+      }
+      if (currentIndex >= 1) {
+        nextButton.classList.remove("inactive");
+      }
+      currentIndex = currentIndex - 1;
+      if (currentIndex === 1) {
+        prevButton.classList.add("inactive");
+      }
+      imageItem.setAttribute("src", `/media/image${currentIndex}.jpg`);
+    } else if (action === "next") {
+      if (currentIndex === numberOfImages) {
+        return;
+      }
+      if (currentIndex !== 0) {
+        prevButton.classList.remove("inactive");
+      }
+      currentIndex = currentIndex + 1;
+      if (currentIndex === numberOfImages) {
+        nextButton.classList.add("inactive");
+      }
+      imageItem.setAttribute("src", `/media/image${currentIndex}.jpg`);
+    }
+  },
+  true
+);
