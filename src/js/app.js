53558d27ae8a52a9593f158785a8721bc413bdc8
soredake <5204968+soredake@users.noreply.github.com>
2023-02-23 19:23:05 +0200

    начало

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,32 @@
 "use strict";
 
+const modal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal-close-button");
 const productObj = {
   name: "Название товара",
   price: "Цена",
   info: "Информация о товаре",
 };
 
+modalCloseButton.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 window.addEventListener("load", function () {
   showCategories();
 });
+
+function addHtml(text, value) {
+  document.getElementsByClassName(
+    "registration_results"
+  )[0].innerHTML += `<p><b>${text}</b>: ${value}</p>`;
+}
 
 function showCategories() {
   const parent = document.getElementById("categories");
@@ -78,11 +96,9 @@ function showInfo(product) {
   }
   const buyButton = document.createElement("button");
   buyButton.innerHTML = "Купить товар";
+  buyButton.classList.add("buyButton");
   buyButton.addEventListener("click", function () {
-    alert(`Вы успешно купили: ${product.name}`);
-    eraseDiv("products");
-    eraseDiv("info");
-    changeActiveAttribute("category");
+    modal.style.display = "block";
   });
   parent.appendChild(buyButton);
 }
@@ -109,3 +125,59 @@ function eraseDiv(id) {
   const element = document.getElementById(id);
   element.innerHTML = "";
 }
+
+document.querySelector(".confirmOrder").addEventListener("click", function () {
+  const requiresFilling = [];
+  const nameLength = document.querySelector('input[name="name"]:invalid');
+  const surnameLength = document.querySelector('input[name="surname"]:invalid');
+  const birthdayLength = document.querySelector(
+    'input[name="birthday"]:invalid'
+  );
+  const addressLength = document.querySelector(
+    'textarea[name="address"]:invalid'
+  );
+  const checkedLanguagesLength = document.querySelectorAll(
+    'input[name="languages"]:checked'
+  ).length;
+  const results = document.getElementsByClassName("results")[0];
+  const registration_results = document.getElementsByClassName(
+    "registration_results"
+  )[0];
+  const form = document.forms.registration;
+  const languagesElements = form.languages;
+  const checkedLanguages = [];
+  const city = form.city;
+
+  nameLength && requiresFilling.push("имя");
+  surnameLength && requiresFilling.push("фамилия");
+  birthdayLength && requiresFilling.push("день рождения");
+  addressLength && requiresFilling.push("адрес");
+  !checkedLanguagesLength && requiresFilling.push("язык");
+
+  if (requiresFilling.length) {
+    requireInput(requiresFilling.join(", "));
+    return;
+  }
+
+  for (let index = 0; index < languagesElements.length; index++) {
+    if (languagesElements[index].checked) {
+      checkedLanguages.push(languagesObj[languagesElements[index].value]);
+    }
+  }
+
+  if (registration_results.innerHTML) {
+    registration_results.innerHTML = "";
+  }
+
+  if (!results.style.visibility) {
+    results.style.visibility = "visible";
+  }
+
+  addHtml("Имя", form.elements.name.value);
+  addHtml("Фамилия", form.elements.surname.value);
+  addHtml("Дата рождения", form.elements.birthday.value);
+  addHtml("Пол", genderObj[form.elements.gender.value]);
+  addHtml("Языки", checkedLanguages.join(", "));
+  addHtml("Адрес", form.elements.address.value);
+  addHtml("Город", city.options[city.selectedIndex].textContent);
+});
