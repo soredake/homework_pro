61c348721723e4c1388668bb9c4ff43b02908bfb
soredake <5204968+soredake@users.noreply.github.com>
2023-02-26 19:40:33 +0200

    продвижение

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,65 @@
 "use strict";
 
-const modal = document.querySelector(".modal");
-const modalCloseButton = document.querySelector(".modal-close-button");
+const order = document.querySelector(".orderBg");
+// const orderCloseButton = document.querySelector(".orderClose");
+
+const orderDetails = document.querySelector(".orderDetailsBg");
+// const orderDetailsCloseButton = document.querySelector(".orderClose");
+
 const productObj = {
   name: "Название товара",
   price: "Цена",
   info: "Информация о товаре",
 };
 
-modalCloseButton.onclick = function () {
-  modal.style.display = "none";
+const cityObj = {
+  odessa: "Одесса",
+  kyiv: "Киев",
+  lviv: "Львов",
+  ivanofrankovsk: "Ивано-Франковск",
 };
 
+const paymentObj = {
+  cod: "Наложенный платёж",
+  creditcard: "Банковская карта",
+};
+
+// orderCloseButton.onclick = function () {
+//   order.style.display = "none";
+// };
+
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  console.log(`Сейчас target это: ${event.target}`);
+  if (event.target == order) {
+    order.style.display = "none";
+    console.log("Click on orderBg");
+  }
+  if (event.target == orderDetails) {
+    orderDetails.style.display = "none";
+    console.log("Click on orderDetailsBg");
   }
 };
+
+// window.onclick = function (event) {
+//   console.log(`Сейчас target это: ${event.target}`);
+//   if (event.target == orderDetails) {
+//     orderDetails.style.display = "none";
+//     console.log("Click on orderDetailsBg");
+//   }
+// };
 
 window.addEventListener("load", function () {
   showCategories();
 });
 
 function addHtml(text, value) {
-  document.getElementsByClassName(
-    "registration_results"
-  )[0].innerHTML += `<p><b>${text}</b>: ${value}</p>`;
+  document.querySelector(
+    ".orderDetailsContent"
+  ).innerHTML += `<p><b>${text}</b>: ${value}</p>`;
+}
+
+function requireInput(requiresFilling) {
+  alert(`Введите: ${requiresFilling}`);
 }
 
 function showCategories() {
@@ -98,7 +132,7 @@ function showInfo(product) {
   buyButton.innerHTML = "Купить товар";
   buyButton.classList.add("buyButton");
   buyButton.addEventListener("click", function () {
-    modal.style.display = "block";
+    order.style.display = "block";
   });
   parent.appendChild(buyButton);
 }
@@ -130,54 +164,68 @@ document.querySelector(".confirmOrder").addEventListener("click", function () {
   const requiresFilling = [];
   const nameLength = document.querySelector('input[name="name"]:invalid');
   const surnameLength = document.querySelector('input[name="surname"]:invalid');
-  const birthdayLength = document.querySelector(
-    'input[name="birthday"]:invalid'
+  const patronymicLength = document.querySelector(
+    'input[name="patronymic"]:invalid'
   );
-  const addressLength = document.querySelector(
-    'textarea[name="address"]:invalid'
+  const deliveryLocationLength = document.querySelector(
+    'input[name="deliveryLocation"]:invalid'
   );
-  const checkedLanguagesLength = document.querySelectorAll(
-    'input[name="languages"]:checked'
-  ).length;
-  const results = document.getElementsByClassName("results")[0];
-  const registration_results = document.getElementsByClassName(
-    "registration_results"
-  )[0];
-  const form = document.forms.registration;
-  const languagesElements = form.languages;
-  const checkedLanguages = [];
+  const quantityLength = document.querySelector(
+    'input[name="quantity"]:invalid'
+  );
+  // const commentaryLength = document.querySelector(
+  //   'input[name="commentary"]:invalid'
+  // );
+  // const birthdayLength = document.querySelector(
+  //   'input[name="birthday"]:invalid'
+  // );
+
+  // const checkedLanguagesLength = document.querySelectorAll(
+  //   'input[name="languages"]:checked'
+  // ).length;
+  const orderDetailsContent = document.querySelector(".orderDetailsContent");
+  // const registration_results = document.getElementsByClassName(
+  //   "registration_results"
+  // )[0];
+  const form = document.forms.orderConfirmation;
+  // const cityElements = form.city;
+  // const checkedLanguages = [];
   const city = form.city;
 
   nameLength && requiresFilling.push("имя");
   surnameLength && requiresFilling.push("фамилия");
-  birthdayLength && requiresFilling.push("день рождения");
-  addressLength && requiresFilling.push("адрес");
-  !checkedLanguagesLength && requiresFilling.push("язык");
+  patronymicLength && requiresFilling.push("отчество");
+  deliveryLocationLength && requiresFilling.push("адрес");
+  quantityLength && requiresFilling.push("количество");
+  // commentaryLength && requiresFilling.push("комментарий");
+  // !checkedLanguagesLength && requiresFilling.push("язык");
 
   if (requiresFilling.length) {
     requireInput(requiresFilling.join(", "));
     return;
   }
 
-  for (let index = 0; index < languagesElements.length; index++) {
-    if (languagesElements[index].checked) {
-      checkedLanguages.push(languagesObj[languagesElements[index].value]);
-    }
+  // for (let index = 0; index < languagesElements.length; index++) {
+  //   if (languagesElements[index].checked) {
+  //     checkedLanguages.push(languagesObj[languagesElements[index].value]);
+  //   }
+  // }
+
+  if (orderDetailsContent.innerHTML) {
+    orderDetailsContent.innerHTML = "";
   }
 
-  if (registration_results.innerHTML) {
-    registration_results.innerHTML = "";
-  }
-
-  if (!results.style.visibility) {
-    results.style.visibility = "visible";
-  }
+  // if (!orderDetailsContent.style.visibility) {
+  //   orderDetailsContent.style.visibility = "visible";
+  // }
 
   addHtml("Имя", form.elements.name.value);
   addHtml("Фамилия", form.elements.surname.value);
-  addHtml("Дата рождения", form.elements.birthday.value);
-  addHtml("Пол", genderObj[form.elements.gender.value]);
-  addHtml("Языки", checkedLanguages.join(", "));
-  addHtml("Адрес", form.elements.address.value);
-  addHtml("Город", city.options[city.selectedIndex].textContent);
+  addHtml("Отчество", form.elements.patronymic.value);
+  addHtml("Город", cityObj[form.elements.city.value]);
+  addHtml("Адрес доставки", form.elements.deliveryLocation.value);
+  addHtml("Способ оплаты", paymentObj[form.elements.payment.value]);
+  addHtml("Количество", form.elements.quantity.value);
+  addHtml("Комментарий к заказу:", form.elements.commentary.value);
+  orderDetails.style.display = "block";
 });
