69582c83476994f68f94908cf9ff25fc13f6ea1c
soredake <5204968+soredake@users.noreply.github.com>
2023-03-14 17:58:16 +0200

    начало

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const modalButton = document.querySelector('[data-bs-toggle="modal"]');
+const showAlertButton = document.getElementById("showAlert");
+const tooltipOptions = { title: "Tooltip text.", trigger: "hover" };
+const tooltip = new bootstrap.Tooltip(modalButton, tooltipOptions);
+const alertElement = document.getElementById("alertElement");
+
+const alert = (type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>Ничего себе! Фантастика!</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+
+  alertElement.append(wrapper);
+};
+
+showAlertButton.addEventListener("click", () => {
+  if (alertElement.innerHTML) {
+    alertElement.innerHTML = "";
+    return;
+  }
+  alert("success");
+});
