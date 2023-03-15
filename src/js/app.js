bf80f664d8ac681cf6687d70562c3a35d80a9bf9
soredake <5204968+soredake@users.noreply.github.com>
2023-03-15 15:16:50 +0200

    15:16

--- src/js/app.js
+++ src/js/app.js
@@ -1,53 +1,83 @@
 "use strict";
 
-const modalButton = document.querySelector('[data-bs-toggle="modal"]');
+// Bootstrap
+const openModal = document.querySelector('[data-bs-toggle="modal"]');
 const showAlertButton = document.getElementById("showAlert");
-const tooltipOptions = { title: "Замечательный tooltip", trigger: "hover" };
-const tooltip = new bootstrap.Tooltip(modalButton, tooltipOptions);
-const alertElement = document.getElementById("alertElement");
-const birthDateElement = document.querySelector(".birthDate");
-const birthDateInput = document.querySelector('input[name="birthDateInput"]');
-const birthDateInputButton = document.querySelector(".birthDateInputButton");
-const birthDate = new Date("1997/2/19");
-const dateInAnotherFormat = document.querySelector(".dateInAnotherFormat");
-
-const alert = (type) => {
+const tooltip = new bootstrap.Tooltip(openModal, {
+  title: "Замечательный tooltip",
+  trigger: "hover",
+});
+const bootstrapAlert = document.getElementById("alertElement");
+const alert = (type, message, alertElement) => {
   const wrapper = document.createElement("div");
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">
-       <div>Ничего себе! Фантастика!</div>
+  wrapper.innerHTML = `<div class="alert alert-${type} alert-dismissible" role="alert">
+       <div>${message}</div>
        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>`,
-  ].join("");
+    </div>`;
 
   alertElement.append(wrapper);
 };
 
 showAlertButton.addEventListener("click", () => {
-  if (alertElement.innerHTML) {
-    alertElement.innerHTML = "";
+  if (bootstrapAlert.innerHTML) {
+    bootstrapAlert.innerHTML = "";
     return;
   }
-  alert("success");
+  alert("success", "Ничего себе! Фантастика!", bootstrapAlert);
 });
 
+// Moment.js
+const myBirthDate = new Date("1997/2/19");
+const myBirthDateElement = document.querySelector(".myBirthDate");
+const birthDateInput = document.querySelector('input[name="birthDateInput"]');
+const formatDate = document.querySelector(".formatDate");
+const dateAlert = document.querySelector(".dateAlert");
+const birthDayFormat = "DD MMMM YYYY";
 moment.locale("ru");
-birthDateElement.innerHTML = `<b>Дата моего рождения:</b> ${moment(
-  birthDate
-).format("DD MMMM YYYY")} года <br><b>В формате UNIX:</b>
-  ${moment(birthDate).format("x")}`;
+myBirthDateElement.innerHTML = `<b>Дата моего рождения:</b> ${moment(
+  myBirthDate
+).format(birthDayFormat)} года<br><b>В формате UNIX:</b>
+  ${moment(myBirthDate).format("x")}`;
 
-birthDateInputButton.addEventListener("click", function () {
-  dateInAnotherFormat.innerHTML = `Дата в формате "DD MMMM YYYY": <b>
-    ${moment(birthDateInput.value).format("DD MMMM YYYY")}</b>`;
-});
+const checkInputValidity = (el) => el.checkValidity();
+const changeElementDisplay = (el, hide) =>
+  hide === true ? (el.style.display = "none") : (el.style.display = "block");
+const requireCorrectDateInput = () => {
+  // dateAlert.innerHTML = `Введите дату в формате <b>YYYY/MM/DD</b>`;
+  alert("danger", `Введите дату в формате <b>YYYY/MM/DD</b>`, dateAlert);
+  // birthDateInput.classList.add("invalid");
+  // birthDateInput.classList.remove("dateSuccess");
+  // dateAlert.classList.add("invalid");
+  // dateAlert.classList.remove("dateSuccess");
+  // changeElementDisplay(dateAlert);
+};
 
-birthDateInput.addEventListener("change", function (event) {
-  const el = event.target;
-  const isInvalid = el.checkValidity();
-  if (isInvalid === true) {
-    el.classList.add("invalid");
+formatDate.addEventListener("click", () => {
+  if (checkInputValidity(birthDateInput) === false) {
+    requireCorrectDateInput();
   } else {
-    el.classList.remove("invalid");
+    // dateAlert.innerHTML = `Дата в формате "${birthDayFormat}": <b>
+    // ${moment(birthDateInput.value).format(birthDayFormat)} года</b>`;
+    // dateAlert.classList.remove("invalid");
+    // dateAlert.classList.add("dateSuccess");
+    // changeElementDisplay(dateAlert);
+    alert(
+      "success",
+      `${moment(birthDateInput.value).format(birthDayFormat)} года`,
+      dateAlert
+    );
   }
 });
+
+// birthDateInput.addEventListener("change", () => {
+//   if (checkInputValidity(birthDateInput) === false) {
+//     requireCorrectDateInput();
+//   } else {
+//     birthDateInput.classList.remove("invalid");
+//     changeElementDisplay(dateAlert, true);
+//   }
+// });
+
+// window.addEventListener("load", () => {
+//   checkInputValidity(birthDateInput) || birthDateInput.classList.add("invalid");
+// });
