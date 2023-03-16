c226d786b1719490aa51e436e58288ab9d653731
soredake <5204968+soredake@users.noreply.github.com>
2023-03-16 14:20:36 +0200

    начало

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,61 @@
 "use strict";
 
 const images = ["image1", "image2", "image3", "image4", "image5", "image6"];
+const numberOfImages = images.length;
+// const prevButton = document.querySelector("[data-action=prev]");
+// const nextButton = document.querySelector("[data-action=next]");
 let currentIndex = 1;
 
+const changeImage = () => {
+  const imageItem = document.querySelector("img");
+  imageItem.setAttribute("src", `/media/image${currentIndex}.jpg`);
+};
+
+const changeImageTimer = () => {
+  // console.log(numberOfImages);
+  // console.log(`Текущий index: ${currentIndex}`);
+  // const nextIndex = currentIndex + 1;
+  if (currentIndex === numberOfImages) {
+    changeImage();
+    currentIndex = 1;
+    return;
+  } else {
+    changeImage();
+    currentIndex = currentIndex + 1;
+  }
+};
 document.querySelector("main").addEventListener(
   "click",
-  function (event) {
+  (event) => {
     const action = event.target.getAttribute("data-action");
-    const numberOfImages = images.length;
-    const imageItem = document.querySelector("img");
-    const prevButton = document.querySelector("[data-action=prev]");
-    const nextButton = document.querySelector("[data-action=next]");
 
     if (!action) {
       return;
     }
 
+    clearInterval(threeSecondTimer);
+
     if (action === "prev") {
-      currentIndex = currentIndex - 1;
       if (currentIndex === 1) {
-        prevButton.style.visibility = "hidden";
+        currentIndex = numberOfImages;
       }
-      nextButton.style.visibility = "visible";
-      imageItem.setAttribute("src", `/media/image${currentIndex}.jpg`);
+      currentIndex = currentIndex - 1;
+      changeImage();
     } else if (action === "next") {
-      currentIndex = currentIndex + 1;
+      // console.log(currentIndex);
       if (currentIndex === numberOfImages) {
-        nextButton.style.visibility = "hidden";
+        // currentIndex = currentIndex + 1;
+        changeImage();
+        currentIndex = 1;
+        return;
       }
-      prevButton.style.visibility = "visible";
-      imageItem.setAttribute("src", `/media/image${currentIndex}.jpg`);
+      changeImage();
+      currentIndex = currentIndex + 1;
     }
+
+    setInterval(changeImageTimer, 3000);
   },
   true
 );
+
+const threeSecondTimer = setInterval(changeImageTimer, 3000);
