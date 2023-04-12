6e026a9173cebbc01d9dfbd46208eaa2f97410bc
soredake <5204968+soredake@users.noreply.github.com>
2023-04-12 20:49:37 +0300

    20:49

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,21 @@ const gulp = require("gulp");
 const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
 const concat = require("gulp-concat");
+const concatCss = require("gulp-concat-css");
 const sourcemaps = require("gulp-sourcemaps");
 const cleanCSS = require("gulp-clean-css");
 const babel = require("gulp-babel");
 const DIST_FOLDER = "./dist/";
 const JS_SRC_FILES = ["./src/js/*/*.js", "./src/js/*.js"];
 const CSS_SRC_FILES = "./src/css/**/*.css";
+
+const watcher = () => {
+  return gulp.watch([JS_SRC_FILES, CSS_SRC_FILES], copy);
+};
+
+const copy = () => {
+  return gulp.src([JS_SRC_FILES, CSS_SRC_FILES]).pipe(gulp.dest(DIST_FOLDER));
+};
 
 const jsPrep = () => {
   return src(JS_SRC_FILES)
@@ -25,15 +34,13 @@ const jsPrep = () => {
 };
 
 const cssPrep = () => {
-  return (
-    src(CSS_SRC_FILES)
-      .pipe(sourcemaps.init())
-      // .pipe(concatCss("app.min.css"))
-      .pipe(cleanCSS({ compatibility: "ie9", level: 2 }))
-      .pipe(sourcemaps.write())
-      .pipe(rename("styles.min.css"))
-      .pipe(dest(DIST_FOLDER))
-  );
+  return src(CSS_SRC_FILES)
+    .pipe(sourcemaps.init())
+    .pipe(concatCss("app.min.css"))
+    .pipe(cleanCSS({ compatibility: "ie9", level: 2 }))
+    .pipe(sourcemaps.write())
+    .pipe(rename("styles.min.css"))
+    .pipe(dest(DIST_FOLDER));
 };
 
-gulp.task("default", gulp.series(jsPrep, cssPrep));
+gulp.task("default", gulp.series(jsPrep, cssPrep, watcher));
