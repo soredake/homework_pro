3487c869242288d0fbe3d1620999588f67ed096f
soredake <5204968+soredake@users.noreply.github.com>
2023-05-05 11:18:29 +0300

    init

---
+++ src/Product.js
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+import { Button } from "antd";
+import "antd/dist/reset.css";
+import "./MainPage.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
