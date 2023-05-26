90bec2fe92ca66ec11b3802ebc5a75ee192ce487
soredake <5204968+soredake@users.noreply.github.com>
2023-05-26 19:36:23 +0300

    validation

--- src/AddContact/AddContact.js
+++ src/AddContact/AddContact.js
@@ -1,5 +1,6 @@
 import "./AddContact.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function AddContact({
   addContact,
@@ -10,9 +11,26 @@ function AddContact({
   currentPhone,
 }) {
   const navigate = useNavigate();
+  const [isFieldsEmpty, setIsFieldsEmpty] = useState(false);
+
+  const checkFormValidity = () => {
+    const form = document.forms.addContact;
+    let emptyFields = [];
+    ["name", "phone"].map((field) => {
+      if (!form[field].checkValidity()) {
+        form[field].classList.add("invalid");
+        setIsFieldsEmpty(true);
+        emptyFields.push(field);
+      }
+    });
+    return emptyFields;
+  };
+
   const handleAddContactClick = () => {
     const form = document.forms.addContact;
-    if (!editMode) {
+    if (checkFormValidity().length > 0) {
+      return;
+    } else if (!editMode) {
       addContact(form.name.value, form.phone.value);
       navigate("/");
     } else {
@@ -20,6 +38,20 @@ function AddContact({
       closeEditModal();
     }
   };
+
+  const handleInputChange = (e) => {
+    const t = e.target;
+    if (!t.checkValidity()) {
+      t.classList.add("invalid");
+    } else {
+      t.classList.remove("invalid");
+    }
+
+    if (Object.keys(checkFormValidity()).length === 0) {
+      setIsFieldsEmpty(false);
+    }
+  };
+
   const handleCancelClick = () => (editMode ? closeEditModal() : navigate("/"));
 
   return (
@@ -30,7 +62,10 @@ function AddContact({
           type="text"
           name="name"
           id="name"
+          required={true}
+          minLength="5"
           placeholder="Elon Musk"
+          onChange={handleInputChange}
           defaultValue={editMode ? currentName : ""}
         />
         <label htmlFor="phone">Enter phone number:</label>
@@ -38,13 +73,20 @@ function AddContact({
           type="text"
           name="phone"
           id="phone"
+          required={true}
+          minLength="5"
           placeholder="+380639557349"
+          onChange={handleInputChange}
           defaultValue={editMode ? currentPhone : ""}
         />
       </form>
+      <div className="warning">
+        {isFieldsEmpty ? "Some of the inputs are empty" : ""}
+      </div>
       <div className="actions">
         <input
           type="button"
+          disabled={isFieldsEmpty ? true : false}
           value={editMode ? "Save user" : "Add user"}
           onClick={handleAddContactClick}
         />
